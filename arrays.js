a0747aa7c8adbdd9db223e6cb7d4f2e197c5fb80
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 00:43:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,6 +41,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  modifiedArray = array.slice(-1)
+  modifiedArray = array.slice(0, array.length - 1)
   return modifiedArray
 }
