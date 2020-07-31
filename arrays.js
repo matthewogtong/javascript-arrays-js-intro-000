dd0ee253227473deb5d213c62db1d5e60448b273
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 00:23:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  var secondArray = [element, ...array]
+  return secondArray
 }
